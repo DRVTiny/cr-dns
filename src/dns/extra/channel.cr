562bbb4ce7b47b4bbeac47a55230841bef4e64a8
Andrey A. Konovalov <drvtiny@gmail.com>
2019-10-19 01:09:50 +0300

    patch for Channel core class is broken on Crystal 0.31.1, so we have to get rid of it and use core class as is

--- src/dns/extra/channel.cr
+++ src/dns/extra/channel.cr
@@ -1,13 +1,13 @@
-abstract class Channel(T)
-	def receive_with_timeout( seconds : Number )
-		return receive_before( Time.now() + Time::Span.new(0,0,seconds) )
-	end
-	def receive_before( t : Time )
-		puts "receive_before( #{t} )"
-		while empty? && (Time.now() < t)
-			Fiber.yield
-		end
-		puts "drop out"
-		return (empty?) ? nil : self.receive
-	end
-end
+#abstract class Channel(T)
+#	def receive_with_timeout( seconds : Number )
+#		return receive_before( Time.now() + Time::Span.new(0,0,seconds) )
+#	end
+#	def receive_before( t : Time )
+#		puts "receive_before( #{t} )"
+#		while empty? && (Time.now() < t)
+#			Fiber.yield
+#		end
+#		puts "drop out"
+#		return (empty?) ? nil : self.receive
+#	end
+#end
